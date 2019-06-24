3dc25ff5b8aae208321c294189d9d8181df19a38
td10216 <td10216@BKK180535>
2019-06-24 10:59:36 +0200

    fått global counter til å funke

--- src/BKK_lab/decoder.js
+++ src/BKK_lab/decoder.js
@@ -2,10 +2,31 @@ function Decoder(bytes, port) {
 
     function Hex2Bin(hex) { return ("00000000" + (parseInt(hex, 16)).toString(2)).substr(-8); }
     function Bin2Dec(n) { return parseInt(n, 2); }
-    function Dec2Bin(n) { return n.toString(2) }
+    function Dec2Bin(n) {
+        n_string = n.toString(2)
+        if (n_string.length === 8) {
+            return n_string;
+        }
+        else {
+
+            while (n_string.length < 8) {
+                n_string = "0".concat(n_string);
+            }
+            return n_string;
+        }
+
+    }
 
 
-
+    var dict = {
+        0x4E: "0x4E periodic data",
+        0x51: "0x51 Motion TOR1 alarm",
+        0x1F: "0x1f Motion channels configuration",
+        0x20: "0x20 Configuration",
+        0x30: "0x30 keep alive",
+        0x4F: "0x4F Motion presence alaram",
+        0x50: "0x50 Motion luminosity alarm"
+    };
     var param = {
 
         "type": null,
@@ -19,40 +40,39 @@ function Decoder(bytes, port) {
         "luminosity_current_percentage": null
     };
 
-    for (var i = 0; i < bytes.length; i++) {
-
-        if (0x4E === bytes[0]) {
-            param.type = "0x4E Periodic data";
+    for (var frameCode in dict) {
+        if (parseInt(frameCode, 16) === bytes[0]) {
+            param.type = dict[frameCode];
         }
-        var status = Dec2Bin(bytes[1]);
-        param.Frame_Counter = parseInt(status.charAt(0), 2) * Math.pow(2, 2) + parseInt(status.charAt(1), 2) * Math.pow(2, 1) + parseInt(status.charAt(2), 2);
-        //ignorerer foreløpig reserved bit
-        if (parseInt(status.charAt(4)) === 1) {
-            param.configuration_inconsistency = true;
-        } else if (parseInt(status.charAt(4)) === 0) {
-            param.configuration_inconsistency = false;
-        } if (parseInt(status.charAt(5)) === 1) {
-            param.Hardware_error = true;
-        } else if (parseInt(status.charAt(5)) === 0) {
-            param.Hardware_error = false;
-        } if (parseInt(status.charAt(6)) === 1) {
-            param.Low_battery = true;
-        } else if (parseInt(status.charAt(6)) === 0) {
-            param.Low_battery = false;
-        } if (parseInt(status.charAt(7)) === 1) {
-            param.configuration_successfull = true;
-        } else if (parseInt(status.charAt(7)) === 0) {
-            param.configuration_successfull = false;
-        }
-
-
-
-
-
-
-
-
     }
+    var status = Dec2Bin(bytes[1]);
+    param.Frame_Counter = parseInt(status.charAt(0), 2) * Math.pow(2, 2) + parseInt(status.charAt(1), 2) * Math.pow(2, 1) + parseInt(status.charAt(2), 2);
+    //ignorerer foreløpig reserved bit
+    //Sjekker om bit'et Configuration inconsistency er markert
+    if (parseInt(status.charAt(4)) === 1) {
+        param.configuration_inconsistency = true;
+    }
+    //hardware error
+    if (parseInt(status.charAt(5)) === 1) {
+        param.Hardware_error = true;
+    }//Low battery
+    if (parseInt(status.charAt(6)) === 1) {
+        param.Low_battery = true;
+    }//Configuration successful
+    if (parseInt(status.charAt(7)) === 1) {
+        param.configuration_successfull = true;
+    }
+    if (param.type.localeCompare(dict[0x4E])) {
+        //sjekker verdi til global presence counter
+        param.presence_global_counter = Bin2Dec(Dec2Bin(bytes[2]).concat(Dec2Bin(bytes[3])));
+    }
+
+
+
+
+
+
+
 
 
     return param;
